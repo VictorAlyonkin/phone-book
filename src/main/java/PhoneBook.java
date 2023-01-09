3eba4d7bfe5af0ec41db0ede1c2518bf3e0c9e7b
Victor Alyonkin <viktoalenkin@ya.ru>
2023-01-09 15:58:55 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -2,8 +2,19 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class PhoneBook {
+    private Map<String, String> phoneNumbers;
+
+    public PhoneBook() {
+        this.phoneNumbers = new TreeMap<>();
+    }
 
     public boolean add(String name, String number){
+        if (number != null &&
+                number.length() == 11 &&
+                name != null){
+            phoneNumbers.put(name, number);
+            return true;
+        }
         return false;
     }
 }
