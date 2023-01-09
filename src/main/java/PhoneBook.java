3e43340c7fd4facecb4a89eb22eb227a1c4b8f54
Victor Alyonkin <viktoalenkin@ya.ru>
2023-01-09 15:58:04 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,9 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    public boolean add(String name, String number){
+        return false;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,27 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+    public static final  String goodPhoneNumber = "89281112233";
+    public static final  String goodName = "89281112233";
+
+    public static final String badPhoneNumber = "9281112233";
+    public static final String badName= null;
+    public PhoneBook phoneBook;
+
+    @BeforeEach
+    public void init(){
+        phoneBook = new PhoneBook();
+    }
+
+    @Test
+    public void successAdd() {
+        Assertions.assertTrue(phoneBook.add(goodName, goodPhoneNumber));
+    }
+
+    @Test
+    public void notSuccessAdd() {
+        Assertions.assertFalse(phoneBook.add(badName, badPhoneNumber));
+    }
 }
